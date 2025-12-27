63f9de054f6ac175a64827e753cc6fe01c9eda0c
Vinuthak <vinutha.kasaragod@gmail.com>
2025-12-27 22:15:39 +0100

    Firebase setup

---
+++ src/utility/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBpPG9rLb6rBBGm9aXLZfL-AVZzJZwdvuA',
+  authDomain: 'shopopedia-c8ad3.firebaseapp.com',
+  projectId: 'shopopedia-c8ad3',
+  storageBucket: 'shopopedia-c8ad3.firebasestorage.app',
+  messagingSenderId: '597221993546',
+  appId: '1:597221993546:web:c855ffc1da8fc73f670ab3',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export { app as firebaseapp }
