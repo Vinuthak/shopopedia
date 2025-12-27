d386240fd3a7266a0bd721ed3d252ce5935ba5e7
Vinuthak <vinutha.kasaragod@gmail.com>
2025-12-27 15:35:20 +0100

    Header layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
+<template>
+  <Header></Header>
+  <RouterView />
+  <Footer></Footer>
+</template>
+
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/layout/Header.vue'
+import Footer from './components/layout/Footer.vue'
 </script>
-
-<template>
-  Hello World
-  <RouterView />
-</template>
