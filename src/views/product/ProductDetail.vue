e769492cf8a257ed8e64df52295112d83829e856
Vinuthak <vinutha.kasaragod@gmail.com>
2025-12-29 18:42:53 +0100

    Add Product details as model view

--- src/views/product/ProductDetail.vue
+++ src/views/product/ProductDetail.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="modal fade" tabindex="-1" :id="`productDetailModal-${product.id}`">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
+          <h5 class="modal-title">Product Details</h5>
           <button
             type="button"
             class="btn-close"
@@ -12,11 +12,59 @@
           ></button>
         </div>
         <div class="modal-body">
-          <p>Modal body text goes here.</p>
+          <!-- <p>Modal body text goes here.</p> -->
+          <div class="row g-4">
+            <div class="col-md-6">
+              <div class="">
+                <img
+                  :src="product.image || `https://via.placehold.com/400x400`"
+                  class="img-fluid rounded-3"
+                  style="max-height: 400px"
+                />
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <h2 class="text-success">{{ product.name }}</h2>
+              <span v-if="product.isbestseller" class="badge text-bg-warning p-2">
+                <i class="bi bi-star-fill"></i> Bestseller
+              </span>
+              <span class="badge text-bg-warning p-2 mx-2">
+                <i class="bi bi-tag-fill px-1"></i>
+                <span> {{ product.category }}</span>
+              </span>
+              <p class="text-muted">{{ product.description }}</p>
+
+              <div class="h3">
+                <span
+                  class="text-success"
+                  :style="{ textDecoration: product.salePrice ? 'line-through' : 'none' }"
+                >
+                  ${{ product.price }}
+                  <span>/sqft</span>
+                </span>
+                <span class="text-danger" v-if="product.salePrice">
+                  {{ product.salePrice }}$/sqft
+                </span>
+              </div>
+
+              <div class="">
+                <div class="d-flex align-items-center gap-2 mb-2"></div>
+                <div class="d-flex flex-wrap gap-2">
+                  <span
+                    class="badge text-bg-secondary p-2"
+                    v-for="(tag, index) in product.tags"
+                    :key="index"
+                    >{{ tag }}</span
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
+          <a href="mailto:example@gmail.com" class="btn btn-success">Contact us</a>
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
