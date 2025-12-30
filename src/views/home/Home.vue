90cde4965145cbb431b1642bdb6935bd41e6eaaf
Vinuthak <vinutha.kasaragod@gmail.com>
2025-12-30 18:39:10 +0100

    Add sort functionality

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -60,13 +60,16 @@
               data-bs-toggle="dropdown"
             >
               <i class="bi bi-sort-down"></i>
-              <span class="text-capitalize">SORT</span>
+              <span class="text-capitalize">{{ selectedSortOption }}</span>
             </button>
             <ul class="dropdown-menu dropdown-menu-end shadow-sm">
-              <li>
-                <button class="dropdown-item py-2 d-flex align-items-center gap-2">
+              <li v-for="(sort, index) in SORT_OPTIONS" :key="index">
+                <button
+                  class="dropdown-item py-2 d-flex align-items-center gap-2"
+                  @click="selectedSortOption = sort"
+                >
                   <i class="bi"></i>
-                  <span class="text-capitalize"> SORT OPTIONS </span>
+                  <span class="text-capitalize"> {{ sort }} </span>
                 </button>
               </li>
             </ul>
@@ -94,14 +97,21 @@
 import { onMounted, ref, computed } from 'vue'
 import productService from '@/services/productService'
 import ProductCard from '../product/ProductCard.vue'
-import { PRODUCT_CATEGORIES } from '@/constants/appConstants'
+import {
+  PRODUCT_CATEGORIES,
+  SORT_NAME_A_Z,
+  SORT_OPTIONS,
+  SORT_NAME_Z_A,
+  SORT_PRICE_HIGH_LOW,
+  SORT_PRICE_LOW_HIGH,
+} from '@/constants/appConstants'
 
 const products = ref([])
 const loading = ref(false)
 const selectedCategory = ref('All')
 const categoryList = ref(['All', ...PRODUCT_CATEGORIES])
 const searchValue = ref('')
-
+const selectedSortOption = ref(SORT_OPTIONS[0])
 onMounted(() => {
   fetchProducts()
 })
@@ -131,6 +141,21 @@ const filteredProductList = computed(() => {
       return item.name.toUpperCase().includes(searchValue.value.toUpperCase())
     })
   }
+
+  if (selectedSortOption.value === SORT_NAME_A_Z) {
+    tempArray.sort((a, b) => a.name.localeCompare(b.name))
+    console.log('clicked  A_Z')
+  }
+  if (selectedSortOption.value === SORT_NAME_Z_A) {
+    tempArray.sort((a, b) => b.name.localeCompare(a.name))
+  }
+  if (selectedSortOption.value === SORT_PRICE_HIGH_LOW) {
+    tempArray.sort((a, b) => b.price - a.price)
+  }
+  if (selectedSortOption.value === SORT_PRICE_LOW_HIGH) {
+    tempArray.sort((a, b) => a.price - b.price)
+  }
+
   return tempArray
 })
 </script>
